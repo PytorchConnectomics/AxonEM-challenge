8d9132c7c978a26c4300b3ba11a20ab960661e67
donglaiw <donglai@holylogin04.rc.fas.harvard.edu>
2021-02-21 21:29:52 -0500

    use erl fork for merge threshold

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ pip3 install kimimaro
 - Example: `python generate_skel.py snemi_train-labels.tif 30x6x6 1,2,3 tmp/gt_skel.p`
 
 ### ERL Evaluation
-- install [funlib.evaluate](https://github.com/funkelab/funlib.evaluate)
+- install [funlib.evaluate](https://github.com/donglaiw/funlib.evaluate)
 ```
 conda env create -n erl-eval
 source activate erl-eval
--- eval_erl.py
+++ eval_erl.py
@@ -25,33 +25,40 @@ def skelToNetworkX(nodes, edges, seg_list, res):
             gt_graph.add_edge(edge[l,0], edge[l,1])
     return gt_graph, node_segment_lut
 
-def test_erl(gt_nodes, gt_edges, pred_seg, res= [30,6,6]):
+def test_erl(gt_nodes, gt_edges, pred_seg, res= [30,6,6], merge_thres=0):
     gt_graph, node_segment_lut = skelToNetworkX(gt_nodes, gt_edges, [pred_seg], res)
     scores = evaluate.expected_run_length(
                     skeletons=gt_graph,
                     skeleton_id_attribute='skeleton_id',
                     edge_length_attribute='length',
                     node_segment_lut=node_segment_lut[0],
+                    merge_thres=merge_thres,
                     skeleton_position_attributes=['z', 'y', 'x'])
     print('ERL:', scores)
 
 if __name__ == "__main__":
     if len(sys.argv) < 4:
-        print('need three arguments: skel_path, seg_path resolution')
+        print('need three arguments: skel_path, seg_path resolution merge_threshold')
         print('example: python eval_erl.py xx.pkl yy.h5 30x6x6')
         # resolution: xyz-order
     # for nodes position: voxel or physical
     skel_path, seg_path, resolution = sys.argv[1], sys.argv[2], sys.argv[3]
     
     gt_nodes, gt_edges = pickle.load(open(skel_path, 'rb'), encoding="latin1")
-
-    # assume node position is the voxel
+    
+    # threshold for number of merges
+    # to be robust to skeleton points aliasing
+    merge_thres = 0
     if len(sys.argv) > 4:
-        for i in range(len(gt_nodes)):
-            if len(gt_nodes[i])>0:
-                gt_nodes[i] = gt_nodes[i] / resolution
+        merge_thres = int(sys.argv[4])
+    # assume node position is the voxel
+    """
+    for i in range(len(gt_nodes)):
+        if len(gt_nodes[i])>0:
+            gt_nodes[i] = gt_nodes[i] / resolution
+    """
 
     pred_seg = readVol(seg_path)
     resolution = [int(x) for x in resolution.split('x')]
 
-    test_erl(gt_nodes, gt_edges, pred_seg, resolution)
+    test_erl(gt_nodes, gt_edges, pred_seg, resolution, merge_thres)
