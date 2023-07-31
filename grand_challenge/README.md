47e2ac093dee8df17f743dd3e4251611f51ceb02
Jason Adhinarta <jasonice6@gmail.com>
2023-07-31 11:38:57 +0200

    use single docker container

--- grand_challenge/README.md
+++ grand_challenge/README.md
@@ -10,6 +10,8 @@
 │   ├── gt_human_16nm_skel_stats_gc.p
 │   └── gt_mouse_16nm_skel_stats_gc.p
 └── test
-    ├── axonH_gt_16nm.h5
-    └── axonM_gt_16nm.h5
+    ├── 0_human_instance_seg_pred.h5
+    └── 1_mouse_instance_seg_pred.h5
  ```
+
+ Might need to change permissions on `test` because of [SELinux](https://stackoverflow.com/a/24334000/10702372).
--- grand_challenge/evaluation.py
+++ grand_challenge/evaluation.py
@@ -13,15 +13,48 @@ from evalutils.evalutils import (
 
 class AxonEM:
     def __init__(self):
-        self.gt_file = os.path.join(DEFAULT_GROUND_TRUTH_PATH, "test-labels.p")
-        self.input_file = os.path.join(DEFAULT_INPUT_PATH, "test-input.h5")
+        self.num_chunk = 64
+        self.human_gt = os.path.join(
+            DEFAULT_GROUND_TRUTH_PATH, "gt_human_16nm_skel_stats_gc.p"
+        )
+        self.mouse_gt = os.path.join(
+            DEFAULT_GROUND_TRUTH_PATH, "gt_mouse_16nm_skel_stats_gc.p"
+        )
+        self.human_input = os.path.join(
+            DEFAULT_INPUT_PATH, "0_human_instance_seg_pred.h5"
+        )
+        self.mouse_input = os.path.join(
+            DEFAULT_INPUT_PATH, "1_mouse_instance_seg_pred.h5"
+        )
+
+        for gt_file in [self.human_gt, self.mouse_gt]:
+            if not os.path.exists(gt_file):
+                raise FileNotFoundError(f"Ground truth file {gt_file} not found.")
+
+        for input_file in [self.human_input, self.mouse_input]:
+            if not os.path.exists(input_file):
+                raise FileNotFoundError(f"Input file {input_file} not found.")
+
         self.output_file = DEFAULT_EVALUATION_OUTPUT_FILE_PATH
 
     def evaluate(self):
-        scores = test_AxonEM(
-            gt_stats_path=self.gt_file, pred_seg_path=self.input_file, num_chunk=64
+        human_scores = test_AxonEM(
+            gt_stats_path=self.human_gt,
+            pred_seg_path=self.human_input,
+            num_chunk=self.num_chunk,
         )
-        metrics = {"erl": np.mean(scores), "scores": scores}
+        mouse_scores = test_AxonEM(
+            gt_stats_path=self.mouse_gt,
+            pred_seg_path=self.mouse_input,
+            num_chunk=self.num_chunk,
+        )
+        metrics = {
+            "erl": np.mean(human_scores + mouse_scores),
+            "erl_human": np.mean(human_scores),
+            "erl_mouse": np.mean(mouse_scores),
+            "scores_human": human_scores,
+            "scores_mouse": mouse_scores,
+        }
 
         with open(self.output_file, "w") as f:
             f.write(json.dumps(metrics))
